f54084e10e2f9f195426e6febe5cd0b6a9e71f76
Trendinao <100587254+Trendinao@users.noreply.github.com>
2022-01-02 16:13:11 +0000

    v0.6.7

--- cmake/fibolib/fibo.cpp
+++ cmake/fibolib/fibo.cpp
@@ -3,4 +3,11 @@
 using namespace std;
 
 void Fibonacci::calculate(){
-	first = 0, second = 1, thi
+	first = 0, second = 1, third = 0;
+	
+	if(n <= 2){
+		third = n-1;
+	}
+	else{
+		for(i=3; i<=n; i++){
+			third = first + second;
