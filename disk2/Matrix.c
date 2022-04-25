1ef66a1efa35e23cf95c384f76b7503d310a83f0
Trendinao <100587254+Trendinao@users.noreply.github.com>
2022-04-25 20:20:34 +0000

    net: fix a race in dst_release()

---
+++ disk2/Matrix.c
@@ -0,0 +1,35 @@
+
+#include "Matrix.h"
+#include <stdlib.h>
+
+void initMatrix(Matrix* m, size_t initCapa){
+	m->array = (char**)malloc(initCapa * sizeof(char*));
+	memset(m->array, 0, initCapa * sizeof(char*));
+	m->used = 0;
+	m->capa = initCapa;
+}
+
+void insertMatrix(Matrix* m, char* element){
+	if(m->used == m->capa){
+		m->capa *= 2;
+		m->array = (char**)realloc(m->array, m->capa * sizeof(char*));
+	}
+	m->array[m->used] = (char*)malloc(sizeof(element) + 1);
+	memset(m->array[m->used], 0, sizeof(element) + 1);
+	strcpy(m->array[m->used++], element);
+}
+
+void printMatrix(Matrix* m){
+	for(int i = 0; i < m->capa; i++){
+		printf("printMatrix...m->array[%d] = %s\n", i, m->array[i]);
+	}
+}
+
+void freeMatrix(Matrix* m){
+	for(int i = 0; i < m->capa; i++){
+		if(m->array[i] != NULL) free(m->array[i]);
+	} 
+	free(m->array);
+	m->array = NULL;
+	m->used = m->capa = 0;
+}
