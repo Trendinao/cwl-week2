562017cd3878504a405b8910ef4e7b09dc3a5a58
Trendinao <100587254+Trendinao@users.noreply.github.com>
2022-06-04 17:55:30 +0000

    Fixes indentation

--- ex10.c
+++ ex10.c
@@ -7,4 +7,10 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void* do_loop(void *data){
 	int i;
-	for(i = 0; i < 10; i++
+	for(i = 0; i < 10; i++){
+		pthread_mutex_lock(&mutex);
+		printf("loop : %d\n", ncount);
+		ncount++;
+		if(i == 10) return;
+
+		pthrea
