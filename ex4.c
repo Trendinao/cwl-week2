860e4778a4bf587af3e679dad147dc9f1a8e070c
Trendinao <100587254+Trendinao@users.noreply.github.com>
2022-06-26 22:05:01 +0000

    Added license file to spec

--- ex4.c
+++ ex4.c
@@ -6,4 +6,13 @@
 int n=0;
 
 void handler(int signum){
-	printf("signal %d recieved\n", 
+	printf("signal %d recieved\n", signum);
+	n=0;
+}
+
+int main(){
+	signal(SIGINT, handler);
+
+	while(1){
+		printf("working %d\n",n++);
+		sleep(1);
