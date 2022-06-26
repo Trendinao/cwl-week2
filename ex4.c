47cb5181497f43d8035e180b2ca928d106985def
Trendinao <100587254+Trendinao@users.noreply.github.com>
2022-06-26 08:02:56 +0000

    - ‘tidal/#test.tidal#’ - ‘tidal/.#test.tidal’ / ‘tidal/test.tidal’

--- ex4.c
+++ ex4.c
@@ -1,3 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <
+#include <signal.h>
+#include <sys/wait.h>
+#include <unistd.h>
+int n=0;
+
+void handler(int signum){
+	printf("signal %d recieved\n", 
