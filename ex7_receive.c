287f0058e39003307d9bf500ad6e16d090161023
Trendinao <100587254+Trendinao@users.noreply.github.com>
2022-06-29 18:49:05 +0000

    Delete unused method

--- ex7_receive.c
+++ ex7_receive.c
@@ -1,3 +1,11 @@
 #include <errno.h>
 #include <fcntl.h>
-#include <stdio.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#define BUFSIZE 256
+
+int m
