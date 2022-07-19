d3dac693c76760b05690748ed5c7ece481056ee2
Trendinao <100587254+Trendinao@users.noreply.github.com>
2022-07-19 05:57:13 +0000

    Whitespace cleanups.

---
+++ minishell/ex.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void hand(int signo){
+	printf("hehe\n");
+}
+
+
+int main(){
+	
+	struct sigaction act;
+	act.sa_handler = hand;
+	act.sa_flags = 0;
+	sigemptyset(&act.sa_mask);
+	sigaction(SIGINT, &act, NULL);
+
+	while(1);
+
+
+}
