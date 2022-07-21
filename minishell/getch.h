c0534b1e074690726c2b087acac95f9c419aea00
Trendinao <100587254+Trendinao@users.noreply.github.com>
2022-07-21 12:42:26 +0000

    Update install.ps1

--- minishell/getch.h
+++ minishell/getch.h
@@ -7,4 +7,10 @@ int getch(void){
 
 	tcgetattr(0, &save);
 	buf = save;
-	buf.c_lflag 
+	buf.c_lflag &= ~(ICANON|ECHO);
+	buf.c_cc[VMIN] = 1;
+	buf.c_cc[VTIME] = 0;
+	
+	tcsetattr(0, TCSAFLUSH, &buf);
+	ch = getchar();
+	tcsetattr(0, 
