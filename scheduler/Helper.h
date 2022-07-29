0a567b99acc4f58783ed79f1a1806a75b8674946
Trendinao <100587254+Trendinao@users.noreply.github.com>
2022-07-29 18:26:39 +0000

    update copyrights

--- scheduler/Helper.h
+++ scheduler/Helper.h
@@ -1,3 +1,9 @@
 #include <stdbool.h>
 #include <pthread.h>
-#include "MsgQueue
+#include "MsgQueue.h"
+pthread_cond_t static_cond; 
+pthread_mutex_t static_mutex;
+bool is_pushed;
+
+/* join structur */
+typedef struct _JoinS
