4a0c88b723436b142eee0bf5b62fe58fc0209b08
Trendinao <100587254+Trendinao@users.noreply.github.com>
2022-07-30 20:57:24 +0000

    svcrpc: make svc_age_temp_xprts enqueue under sv_lock

--- scheduler/Helper.h
+++ scheduler/Helper.h
@@ -6,4 +6,10 @@ pthread_mutex_t static_mutex;
 bool is_pushed;
 
 /* join structur */
-typedef struct _JoinS
+typedef struct _JoinStr JoinStr;
+typedef struct _JoinStr{
+	pthread_t tid;
+	pthread_t parentTid;
+	JoinStr* pPrev;
+	JoinStr* pNext;
+} JoinStr;
