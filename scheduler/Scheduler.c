65d2a19c8f6b9bc9e4219470c8560ac309ea957a
Trendinao <100587254+Trendinao@users.noreply.github.com>
2022-08-06 11:27:59 +0000

    fixed img

---
+++ scheduler/Scheduler.c
@@ -0,0 +1,30 @@
+
+#include "Init.h"
+#include "Thread.h"
+#include "Scheduler.h"
+#include "Helper.h"
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+
+int RunScheduler( void ){
+	//run scheduler
+	Thread* pCurThread = NULL;
+	Thread* pNewThread = NULL;
+	while(1){
+		fflush(stdout);
+		
+		pCurThread = RunQHead;
+		//get running thread's TCB pointer
+		
+		if(ReadyQHead != NULL){
+			//if ready queue is not empty	
+			if(pCurThread == NULL) //fprintf(stderr, "running thread's TCB isn't exist (RunScheduler())\n");
+			if((pNewThread = ReadyQHead) == NULL){
+				//check any TCB is in ready queue
+				continue;
+				//if ready queue is empty, don't call context switch
+			}
+		}
+		__ContextSwitch(pCurThread, pNewThread);
+		//context switch
