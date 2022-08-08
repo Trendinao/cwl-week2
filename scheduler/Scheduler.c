4f18530e474ebf7d7405c371936d94c21b9de784
Trendinao <100587254+Trendinao@users.noreply.github.com>
2022-08-08 08:56:58 +0000

    Update Android.md

--- scheduler/Scheduler.c
+++ scheduler/Scheduler.c
@@ -28,3 +28,37 @@ int RunScheduler( void ){
 		}
 		__ContextSwitch(pCurThread, pNewThread);
 		//context switch
+		
+		sleep(TIMESLICE);
+	}
+}
+
+
+void __ContextSwitch(Thread* pCurThread, Thread* pNewThread){
+	//send running thread to ready queue, pop head thread and run
+	//schduler call rq_pop, pass Thread pointer to __ContextSwitch()
+
+	if(pCurThread != NULL){
+		//if current thread is exist
+		pCurThread->status = THREAD_STATUS_READY;
+		pCurThread->bRunnable = false;
+		//set stoped thread's status to ready
+		
+		pthread_kill(pCurThread->tid, SIGUSR1);
+		//stop running thread
+		
+		rq_push(pCurThread);
+		//push stoped thread at ready queue
+	}
+
+	if(pNewThread != NULL){
+		//if new thread is exist
+		
+		pNewThread = rq_pop();
+		__thread_wakeup(pNewThread);
+		//run new thread by waking up
+		
+		RunQHead = pNewThread;
+		//for track running thread's TCB
+	}
+}
---
+++ scheduler/TestCase1.h
@@ -0,0 +1,5 @@
+#ifndef TEST_CASE_1_H
+#define TEST_CASE_1_H
+ 
+#include <stdio.h>
+#include <unistd.h>
