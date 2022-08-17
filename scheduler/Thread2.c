9dc00cccca85dc1afe686f28e52e49452b369e3e
Trendinao <100587254+Trendinao@users.noreply.github.com>
2022-08-17 19:16:37 +0000

    ignore dist

--- scheduler/Thread2.c
+++ scheduler/Thread2.c
@@ -173,3 +173,119 @@ void __thread_wakeup(Thread* pTCB){
 
 void rq_push(Thread *in_TCB){
 	//insert in_TCB at ready queue
+	if(ReadyQHead == NULL){
+		//if ready queue is empty
+		ReadyQHead = in_TCB;
+		ReadyQTail = in_TCB;
+	}
+	else{
+		//if ready queue is not empty
+		in_TCB->pPrev = ReadyQTail;
+		//in_TCB link to previous TCB
+		ReadyQTail->pNext = in_TCB;
+		//previous TCB link to in_TCB
+		ReadyQTail = ReadyQTail->pNext;
+		//advance ReadyQTail
+	}
+}
+
+Thread* rq_search(pthread_t s_tid){
+	//search TCB by tid in ready queue
+	Thread* tmp;
+	tmp = ReadyQHead;
+	
+	while(tmp != NULL){ //or tmp != readyQTail
+		//search until last TCB
+		if(tmp->tid == s_tid){
+			return tmp;
+		}
+		tmp = tmp->pNext;
+		//advance tmp
+	}
+	
+	return NULL;
+	//if not founded
+}
+
+Thread* rq_remove(pthread_t r_tid){
+	//remove corresponding TCB from ready queue
+	Thread* tmp;
+	
+	if((tmp = rq_search(r_tid)) == NULL){
+		perror("not exist searching TCB : ");
+		return NULL;
+	}
+	printf("exist searching TCB (tid = %lu)\n",r_tid);
+	
+	if(tmp->pPrev != NULL) tmp->pPrev->pNext = tmp->pNext;
+	//previous TCB link to next TCB
+	if(tmp->pNext != NULL) tmp->pNext->pPrev = tmp->pPrev;
+	//next TCB link to previous TCB
+	printf("link prev and next complete\n");
+
+	tmp->pPrev = NULL;
+	tmp->pNext = NULL;
+	//init tmp
+	
+	return tmp;
+}
+
+Thread* rq_pop(){
+	//pop head TCB from ready queue
+	Thread* tmp = ReadyQHead;
+
+	if(tmp == NULL){
+		perror("not exist TCB in ready queue : ");
+		return NULL;
+	}
+	if(tmp->pNext != NULL) tmp->pNext->pPrev = NULL;
+	//next TCB unlink to tmp
+	ReadyQHead = tmp->pNext;
+	//advance readyQHead
+	tmp->pNext = NULL;
+	//tmp unlink to next TCB
+	
+	return tmp;
+}
+
+void print_rq(){
+	Thread* iter = ReadyQHead;
+	while(iter != NULL){
+		printf("tid = %lu\n", iter->tid);
+		iter = iter->pNext;
+
+	}
+	printf("print ready queue end\n");
+}
+
+void wq_push(Thread *in_TCB){
+	//insert in_TCB at wait queue
+	if(WaitQHead == NULL){
+		//if wait queue is empty
+		WaitQHead = in_TCB;
+		WaitQTail = in_TCB;
+	}
+	else{
+		//if wait queue is not empty
+		in_TCB->pPrev = WaitQTail;
+		//in_TCB link to previous TCB
+		WaitQTail->pNext = in_TCB;
+		//previous TCB link to in_TCB
+		WaitQTail = WaitQTail->pNext;
+		//advance WaitQTail
+	}
+}
+
+Thread* wq_search(pthread_t s_tid){
+	//search TCB by tid in wait queue
+	Thread* tmp;
+	tmp = WaitQHead;
+	
+	while(tmp != NULL){ //or tmp != WaitQTail
+		//search until last TCB
+		if(tmp->tid == s_tid){
+			return tmp;
+		}
+		tmp = tmp->pNext;
+		//advance tmp
+	}
