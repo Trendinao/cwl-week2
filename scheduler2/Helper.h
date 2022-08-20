2dc974ee3739e480409c7376ea20f09e52279140
Trendinao <100587254+Trendinao@users.noreply.github.com>
2022-08-20 12:42:02 +0000

    <3

--- scheduler2/Helper.h
+++ scheduler2/Helper.h
@@ -2,4 +2,9 @@
 #include <pthread.h>
 #include "MsgQueue.h"
 pthread_cond_t static_cond; 
-pthread_mutex_t
+pthread_mutex_t static_mutex;
+bool is_pushed;
+
+/* join structur */
+typedef struct _JoinStr JoinStr;
+typedef struct
