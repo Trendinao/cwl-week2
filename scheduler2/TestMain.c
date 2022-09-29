a94fa2b290328d3f7da98b93b283482fb8040647
Trendinao <100587254+Trendinao@users.noreply.github.com>
2022-09-29 15:32:29 +0000

    Update Czech translation

--- scheduler2/TestMain.c
+++ scheduler2/TestMain.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
-#include <stdlib
+#include <stdlib.h>
+
+#include "TestCase1.h"
+#include "TestCase2.h"
+#include "TestCase3.h"
+#include "TestCase4.h"
+
+
+#include "Init.h"
+#i
