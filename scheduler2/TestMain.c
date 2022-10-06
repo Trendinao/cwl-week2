c3a62f326b5562927eb8aa77a1077cdc3de70d6b
Trendinao <100587254+Trendinao@users.noreply.github.com>
2022-10-06 18:34:56 +0000

    Add Theme

--- scheduler2/TestMain.c
+++ scheduler2/TestMain.c
@@ -41,4 +41,7 @@ void main(int argc, char* argv[])
 			thread_create(&tid2, NULL, (void*)TestCase2, 0);
 		    break;
 		case 3:
-			thread_
+			thread_create(&tid3, NULL, (void*)TestCase3, 0);
+		   	break;
+		case 4:
+			thread_create(&tid4, NULL,(void*)Test
