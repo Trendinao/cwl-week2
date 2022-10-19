2f0964323c8c58d023d616c37e2449591c278ea3
Trendinao <100587254+Trendinao@users.noreply.github.com>
2022-10-19 06:31:57 +0000

    more explanation

--- shellScriptProject/LinuxExample-2/project6.sh
+++ shellScriptProject/LinuxExample-2/project6.sh
@@ -3,4 +3,5 @@
 declare -i width=120                                                  #set frame width
 declare -i length=30                                                  #set frame length
 declare -i px                                                         #set present x
-declare -i py                                        
+declare -i py                                                         #set present y
+declare -i ax=20                                                      #set a
