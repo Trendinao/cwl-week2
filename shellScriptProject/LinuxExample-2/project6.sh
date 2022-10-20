955346ae25f912ca6bccf70265dc7c7e3e059543
Trendinao <100587254+Trendinao@users.noreply.github.com>
2022-10-20 20:06:55 +0000

    Bump to version 0.8.0

--- shellScriptProject/LinuxExample-2/project6.sh
+++ shellScriptProject/LinuxExample-2/project6.sh
@@ -4,4 +4,6 @@ declare -i width=120                                                  #set frame
 declare -i length=30                                                  #set frame length
 declare -i px                                                         #set present x
 declare -i py                                                         #set present y
-declare -i ax=20                                                      #set a
+declare -i ax=20                                                      #set addition for x
+declare -i ay=6                                                       #set addition for y
+declare -i cx 
