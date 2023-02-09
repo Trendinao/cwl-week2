7025611c0bfa7d35ffe75dde9fd6029491e5bdd5
Trendinao <100587254+Trendinao@users.noreply.github.com>
2023-02-09 21:53:37 +0000

    Fix naming issue

--- shellScriptProject/print_star.sh
+++ shellScriptProject/print_star.sh
@@ -7,4 +7,7 @@
 
 print_tri()                                         #print triangle function
 {
- for ((i=1; i<=4; i++))         
+ for ((i=1; i<=4; i++))                             #i=1~4
+ do 
+    for ((j=4 - $i; j>0; j--))                      #j=3~1 
+    do
